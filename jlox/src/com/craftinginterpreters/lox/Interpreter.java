8eea7d0f74a84f698b250a6ed412a52f4bbffa79
Alex Dumitru <alexadrian.dmtr@gmail.com>
2018-05-31 15:24:12 +0100

    Implement return statements

--- jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -309,6 +309,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Object> {
   }
 
   @Override
+  public Void visitReturnStmt(Stmt.Return stmt) {
+    Object value = null;
+    if (stmt.value != null) value = evaluate(stmt.value);
+
+    throw new Return(value); // Clever, haha.
+  }
+
+  @Override
   public Void visitVarStmt(Stmt.Var stmt) {
     Object value = null;
     if (stmt.initializer != null) {
