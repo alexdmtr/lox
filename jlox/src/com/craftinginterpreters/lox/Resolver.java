bd96169dfaba705f91320c106770af53874a1f36
Alex Dumitru <alexadrian.dmtr@gmail.com>
2018-06-08 22:38:22 +0300

    Add static analysis for "super"

--- jlox/src/com/craftinginterpreters/lox/Resolver.java
+++ jlox/src/com/craftinginterpreters/lox/Resolver.java
@@ -23,7 +23,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum ClassType {
     NONE,
-    CLASS
+    CLASS,
+    SUBCLASS
   }
 
   private enum FunctionContext {
@@ -62,6 +63,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     currentClass = ClassType.CLASS;
 
     if (stmt.superclass != null) {
+      currentClass = ClassType.SUBCLASS;
       resolve(stmt.superclass);
       beginScope();
       scopes.peek().put("super", true);
@@ -150,6 +152,13 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitSuperExpr(Expr.Super expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword,
+          "Cannot use 'super' outside of a class.");
+    } else if (currentClass != ClassType.SUBCLASS) {
+      Lox.error(expr.keyword,
+          "Cannot use 'super' in a class with no superclass.");
+    }
     resolveLocal(expr, expr.keyword);
     return null;
   }
